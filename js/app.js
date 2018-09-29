d234e0f8a188c4095d2ba2951ea8a977e0ae607f
Dmitry Sharabin <dmitrysharabin@gmail.com>
2018-09-29 23:53:32 +0300

    feat: Add Player class

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ class Enemy {
     constructor() {
       // Variables applied to each of our instances go here
 
-      // Place enemy off the canvas (to the left) at random position
+      // Place enemy off the screen (to the left) at random position
       this.x = -101 - Math.random() * (100);
       // at random row
       this.row = Math.floor(Math.random() * 3 + 1);;
@@ -31,9 +31,9 @@ class Enemy {
       // all computers.
       this.x = this.x + this.speed;
 
-      // If enemy's off the canvas
+      // If enemy's off the screen
       if(this.x >= 505) {
-        // place it to the left of the canvas at random position
+        // place it to the left of the screen at random position
         this.x = -101 - Math.random() * 100;
         // at random row
         this.row = Math.floor(Math.random() * 3 + 1);
@@ -47,16 +47,71 @@ class Enemy {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+class Player {
+  constructor() {
+    this.col = 2;
+    this.row = 5;
+    this.x = this.col * 101;
+    this.y = this.row * 83 - 23;
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+  }
 
+  // Draw the player on the screen, required method for game
+  render() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  // Update the player's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update(dt) {
+    this.x = this.col * 101;
+    this.y = this.row * 83 - 23;
+  }
+
+  handleInput(direction) {
+    // The player cannot move off the screen
+    switch (direction) {
+      case 'left':
+        if(this.col > 0) { this.col -= 1; }
+        break;
+      case 'up':
+        if(this.row > 0) {
+          this.row -= 1;
+          if (this.row === 0) {
+            setTimeout(() => {
+              this.reset();
+            }, 100);
+          }
+        }
+        break;
+      case 'right':
+        if(this.col < 4) { this.col += 1; }
+        break;
+      case 'down':
+        if(this.row < 5) { this.row += 1; }
+    }
+  }
+  
+  // Return player into his initial position
+  reset() {
+    this.col = 2;
+    this.row = 5;
+    this.x = this.col * 101;
+    this.y = this.row * 83 - 23;
+  }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 const allEnemies = [];
-for(let row = 1; row <= 3; row++) {
+for(let enemy = 1; enemy <= 3; enemy++) {
   allEnemies.push(new Enemy());
 }
 
 // Place the player object in a variable called player
+const player = new Player();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.

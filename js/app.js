e88fde5d8a1a01205fbc16829b59ed0a553ec0a9
Dmitry Sharabin <dmitrysharabin@gmail.com>
2018-09-29 23:14:57 +0300

    feat: Add Enemy class

--- js/app.js
+++ js/app.js
@@ -1,25 +1,48 @@
+'use strict';
+
 // Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
+class Enemy {
+    constructor() {
+      // Variables applied to each of our instances go here
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+      // Place enemy off the canvas (to the left) at random position
+      this.x = -101 - Math.random() * (100);
+      // at random row
+      this.row = Math.floor(Math.random() * 3 + 1);;
+      this.y = this.row * 83 - 23;
+      // and randomly set its speed
+      this.speed = Math.random() * 5 + 1;;
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+      // The image/sprite for our enemies, this uses
+      // a helper we've provided to easily load images
+      this.sprite = 'images/enemy-bug.png';
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+    // Draw the enemy on the screen, required method for game
+    render() {
+      ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt) {
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+      this.x = this.x + this.speed;
+
+      // If enemy's off the canvas
+      if(this.x >= 505) {
+        // place it to the left of the canvas at random position
+        this.x = -101 - Math.random() * 100;
+        // at random row
+        this.row = Math.floor(Math.random() * 3 + 1);
+        this.y = this.row * 83 - 23;
+        // and randomly change its speed
+        this.speed = Math.random() * 5 + 1;
+      }
+    }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -28,9 +51,12 @@ Enemy.prototype.render = function() {
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
+const allEnemies = [];
+for(let row = 1; row <= 3; row++) {
+  allEnemies.push(new Enemy());
+}
+
 // Place the player object in a variable called player
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.

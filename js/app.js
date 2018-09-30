1dc40e276f2eb8eaff711b7405e822c7eae67cf7
Dmitry Sharabin <dmitrysharabin@gmail.com>
2018-09-30 23:23:54 +0300

    docs: Remove unnecessary comments

--- js/app.js
+++ js/app.js
@@ -36,11 +36,7 @@ class Enemy {
     }
 
     // Update the enemy's position
-    // Parameter: dt, a time delta between ticks
     update(dt) {
-      // You should multiply any movement by the dt parameter
-      // which will ensure the game runs at the same speed for
-      // all computers.
       this.x = this.x + this.speed;
 
       // If enemy's off the screen reset it

b988177c783d5044d2bdc892350523901bf30753
Dmitry Sharabin <dmitrysharabin@gmail.com>
2018-09-30 19:19:36 +0300

    fix: Prevent browser window scrolling

--- js/app.js
+++ js/app.js
@@ -253,14 +253,14 @@ const player = new Player('images/char-boy.png', 'Boy');
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
         40: 'down'
     };
-
+    e.preventDefault();
     player.handleInput(allowedKeys[e.keyCode]);
 });
 
